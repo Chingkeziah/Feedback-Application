cd75fba9525081ec2836d943d6b5d6ec48c15a58
Ching Keziah <chingkeziah@gmail.com>
2024-03-07 21:39:44 -0500

    feat: added average of the total review rating

--- src/Components/ReviewStats.jsx
+++ src/Components/ReviewStats.jsx
@@ -1,11 +1,19 @@
 
 
 function ReviewStats({reviews}) {
-    
+    // sum of the total rating and divided by the total length of the array
+    let average = reviews.reduce((acc, cur) => {
+        return acc + cur.rating}, 0)/reviews.length
+
+        // this converts the average output to 1 decimal point
+   average = average.toFixed(1)
   return (
     <>
-      <div className="review-stats"></div>
-        <p>{reviews.length} Reviews</p>
+      <div className="review-stats">
+        <p>Reviews: {reviews.length}</p>
+
+        <p>{isNaN(average) ? 0 : average}</p>
+      </div>
     </>
   )
 }
